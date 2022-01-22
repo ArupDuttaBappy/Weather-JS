b1ec8427b8fccff566fefa3f1031cbaba872d2ff
Arup Dutta Bappy <ad.bappy10@gmail.com>
2022-01-22 19:03:33 +0600

    data populate

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 <h3>Weather Report App</h3><br>
 <a href='https://arupduttabappy.github.io/Weather-JS/'>Github IO</a><br>
+Weather API used: api.openweathermap.org <br>
--- app.js
+++ app.js
@@ -2,7 +2,10 @@ window.addEventListener("load", () => {
   let latitude;
   let longitude;
   const api_key = "fa9453a812eb372fb8ff3d1bf67ed332";
-  let 
+  let region_name = document.querySelector(".region-name");
+  let temperature_value = document.querySelector(".temperature-value");
+  let weather_condition = document.querySelector(".weather-condition");
+  let weather_details = document.querySelector(".weather-details");
 
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(geolocation => {
@@ -15,7 +18,10 @@ window.addEventListener("load", () => {
         })
         .then(data => {
           console.log(data);
-          console.log(data.weather[0].description);
+          region_name.textContent = data.name + ", " + data.sys.country;
+          temperature_value.textContent = data.main.temp;
+          weather_condition.textContent = data.weather.clear;
+          weather_details.textContent = data.weather[0].description + ", temperature feels like " + data.main.feels_like + data.main.humidity + ", wind speed " + data.wind.speed + "km/h";
         })
     });
   }
