f2b49cc990740c825bb8ecbd21ccd6492a0604ec
Arup Dutta Bappy <ad.bappy10@gmail.com>
2022-02-01 16:26:51 +0600

    update

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 window.addEventListener("load", () => {
   const api_key = "fa9453a812eb372fb8ff3d1bf67ed332";
+  const search_city_input = document.getElementById('searchCityInput');
+  const search_city_btn = document.getElementById('searchCityBtn');
+  const get_user_location_btn = document.getElementById('getUserLocationBtn');
 
-  const searchButton = document.getElementById('search-button');
-  const searchInput = document.getElementById('search-input');
-  searchButton.addEventListener('click', () => {
-    const inputValue = searchInput.value;
-    alert(inputValue);
+  search_city_btn.addEventListener("click", function() {
+    if(search_city_input.value == "") {
+      alert("Enter a city name (e.g. dhaka).");
+    }
+    else {
+      let search_box_city_name = search_city_input.value;
+      alert(search_box_city_name);
+    }
+  });
+
+  getUserLocation();
+
+  get_user_location_btn.addEventListener("click", function() {
+    getUserLocation();
   });
 
   function getUserLocation() {
     if (navigator.geolocation) {
-      // returns a coordinates object to the function specified in the parameter -- showPosition
+      // returns a coordinates object to the function specified in the parameter -- getGeoPosition
       navigator.geolocation.getCurrentPosition(getGeoPosition, showGetPositionError);
     } else {
       alert("Geolocation is not supported by this browser.");
@@ -20,6 +32,7 @@ window.addEventListener("load", () => {
   function getGeoPosition(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
+    alert(latitude+", "+longitude)
   }
 
   // user location: handling errors and rejections
